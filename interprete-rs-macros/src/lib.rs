5c868446b67da0f3bc574c13f416f272fb6dc349
Dylan Bulfin <dylanbulfin@gmail.com>
2024-11-08 08:34:40 -0500

    Add macro module, fix gitignore

---
+++ interprete-rs-macros/src/lib.rs
@@ -0,0 +1,27 @@
+macro_rules! bf_instr_metamacro {
+    ( $op:tt ) => {
+
+    };
+}
+
+macro_rules! bf_instr {
+    ( +, $mem:ident ) => {
+        
+    };
+}
+
+macro_rules! bf_interpret {
+    () => {
+        
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        bf_instr!();
+    }
+}
