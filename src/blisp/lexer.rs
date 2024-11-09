15b82c3d10ec9f72e7a302f4eff7254e857b8e62
Dylan Bulfin <dylanbulfin@gmail.com>
2024-11-09 09:52:42 -0500

    Add string/char literal processing code, tests

--- src/blisp/lexer.rs
+++ src/blisp/lexer.rs
@@ -137,7 +137,7 @@ pub enum Token {
     NumLiteral(NumLiteral),
     CharLiteral(u8),
     UnitLiteral,
-    String(String),
+    StringLiteral(String),
     Ident(String),
     Type(Type),
     Plus,
@@ -160,13 +160,63 @@ impl From<NumLiteral> for Token {
     }
 }
 
-fn handle_num_literal(input: &[char]) -> InterpreteResult<(NumLiteral, usize)> {
-    macro_rules! error_template {
-        () => {
-            "Unexpected char while parsing number: {}"
-        };
+// Easier testing
+impl From<&str> for Token {
+    fn from(value: &str) -> Self {
+        Self::StringLiteral(value.to_string())
+    }
+}
+impl From<String> for Token {
+    fn from(value: String) -> Self {
+        Self::StringLiteral(value)
+    }
+}
+
+fn handle_char_literal(input: &[char]) -> InterpreteResult<u8> {
+    // input[0] points at opening `'`
+    // we don't handle escape characters so we can assume that the body of the char literal will
+    // take up exactly one byte of input.
+    if *input
+        .get(2)
+        .ok_or("Reached end of input unexpectedly while parsing a char literal")?
+        != '\''
+    {
+        Err("Did not find closing \' where expected".into())
+    } else if !input[1].is_ascii() {
+        panic!(
+            "Encountered non-ascii character {} while parsing char literal",
+            input[1]
+        )
+    } else {
+        Ok(input[1] as u8)
+    }
+}
+
+fn handle_string_literal(input: &[char]) -> InterpreteResult<String> {
+    // Starting on character directly after opening "
+    let mut curr_index = 1;
+    let mut curr_str = String::new();
+
+    loop {
+        if curr_index >= input.len() {
+            return Err("Unexpectedly reached end of input while parsing a string literal".into());
+        }
+        if !input[curr_index].is_ascii() {
+            return Err("Encountered non-ascii character {} while parsing string literal".into());
+        }
+
+        match input[curr_index] {
+            '\"' => break,
+            c => curr_str.push(c),
+        }
+
+        curr_index += 1;
     }
 
+    Ok(curr_str)
+}
+
+fn handle_num_literal(input: &[char]) -> InterpreteResult<(NumLiteral, usize)> {
     let mut curr_index = 0;
 
     let negative = input[0] == '-';
@@ -176,9 +226,10 @@ fn handle_num_literal(input: &[char]) -> InterpreteResult<(NumLiteral, usize)> {
 
     // This check explicitly ensures we have a digit at the start of the number before the real
     // parsing
-    let mut int_part = input[curr_index]
-        .to_digit(10)
-        .ok_or(format!(error_template!(), input[curr_index]))? as u64;
+    let mut int_part = input[curr_index].to_digit(10).ok_or(format!(
+        "Unexpected char while parsing number: {}",
+        input[curr_index]
+    ))? as u64;
     curr_index += 1;
 
     let mut float = false;
@@ -249,20 +300,32 @@ pub fn tokenize(input: Vec<char>) -> InterpreteResult<Vec<Token>> {
             '+' => res.push(Token::Plus),
             '(' => res.push(Token::LParen),
             ')' => res.push(Token::RParen),
+            '[' => res.push(Token::LBrack),
+            ']' => res.push(Token::RBrack),
             '/' => res.push(Token::Div),
             '*' => res.push(Token::Mult),
-            '\'' => res.push(Token::SingleQuote),
-            '\"' => res.push(Token::DoubleQuote),
             '0'..='9' => {
                 let (lit, count) = handle_num_literal(&input[curr_index..])?;
                 curr_index += count - 1;
                 res.push(Token::NumLiteral(lit))
             }
+            '\'' => {
+                let c = handle_char_literal(&input[curr_index..])?;
+                // Since a char literal takes up 3 characters
+                curr_index += 2;
+                res.push(Token::CharLiteral(c));
+            }
+            '\"' => {
+                let s = handle_string_literal(&input[curr_index..])?;
+                // Need to ultimately shift by s.len() + 2, including standard shift by 1
+                curr_index += s.len() + 1;
+                res.push(Token::StringLiteral(s));
+            }
             '-' => {
-                if input
+                if *input
                     .get(curr_index + 1)
                     .ok_or("Unexpectedly reached end of input")?
-                    == &' '
+                    == ' '
                 {
                     res.push(Token::Minus);
                 } else {
@@ -272,7 +335,7 @@ pub fn tokenize(input: Vec<char>) -> InterpreteResult<Vec<Token>> {
                 }
             }
             ' ' => (),
-            _ => unimplemented!(),
+            c => panic!("Haven't implemented the char {}", c),
         };
 
         curr_index += 1;
@@ -287,38 +350,6 @@ mod tests {
 
     use super::*;
 
-    // This is an attempt at a macro to make forming numeric literals easier but it's a complete
-    // nightmare
-    macro_rules! num_lit {
-        ($(-1$neg:vis,;)? $int:literal $(,$float:literal)? $(;$suffix:ident)?) => {
-            #[allow(unused_mut)]
-            {
-                let mut negative = false;
-                let mut int_part = $int;
-                let mut float = false;
-                let mut dec_part = 0;
-                let mut suffix = LiteralSuffix::None;
-
-                $(
-                    $ignore($neg);
-                    negative = true;
-                    int_part = -1 * int_part;
-                )?
-
-                $(dec_part = $float;)?
-                $(suffix = LiteralSuffix::$suffix;)?
-
-                NumLiteral {
-                    negative,
-                    int_part,
-                    dec_part,
-                    suffix,
-                    float,
-                }
-            }
-        };
-    }
-
     #[test]
     fn parentheses() -> InterpreTestResult {
         let (input1, output1) = (
@@ -332,6 +363,29 @@ mod tests {
                 Token::LParen,
                 Token::LParen,
                 Token::RParen,
+                Token::RParen,
+            ],
+        );
+
+        assert_eq!(tokenize(input1)?, output1);
+        assert_eq!(tokenize(input2)?, output2);
+
+        Ok(())
+    }
+
+    #[test]
+    fn brackets() -> InterpreTestResult {
+        let (input1, output1) = (
+            "([])".chars().collect(),
+            [Token::LParen, Token::LBrack, Token::RBrack, Token::RParen],
+        );
+        let (input2, output2) = (
+            "([[])".chars().collect(),
+            [
+                Token::LParen,
+                Token::LBrack,
+                Token::LBrack,
+                Token::RBrack,
                 Token::RParen,
             ],
         );
@@ -421,6 +475,89 @@ mod tests {
         assert_eq!(tokenize(input1)?, output1);
         assert_eq!(tokenize(input2)?, output2);
         assert_eq!(tokenize(input3)?, output3);
+
+        Ok(())
+    }
+
+    #[test]
+    fn char_literal_test() -> InterpreTestResult {
+        let (input1, output1) = (
+            "(['a' 'n' '?'] 'Z' '0')".chars().collect(),
+            [
+                Token::LParen,
+                Token::LBrack,
+                Token::CharLiteral(b'a'),
+                Token::CharLiteral(b'n'),
+                Token::CharLiteral(b'?'),
+                Token::RBrack,
+                Token::CharLiteral(b'Z'),
+                Token::CharLiteral(b'0'),
+                Token::RParen,
+            ],
+        );
+        let (input2, output2) = (
+            // ''' is valid because of how I naively parse char literals
+            "(- (+ 'a' 1c) '`' ''')".chars().collect(),
+            [
+                Token::LParen,
+                Token::Minus,
+                Token::LParen,
+                Token::Plus,
+                Token::CharLiteral(b'a'),
+                Token::from(NumLiteral::new_int_with_suffix(1, false, 'c')),
+                Token::RParen,
+                Token::CharLiteral(b'`'),
+                Token::CharLiteral(b'\''),
+                Token::RParen,
+            ],
+        );
+
+        assert_eq!(tokenize(input1)?, output1);
+        assert_eq!(tokenize(input2)?, output2);
+
+        Ok(())
+    }
+
+    #[test]
+    fn string_literal_test() -> InterpreTestResult {
+        let (input1, output1) = (
+            "([\"ABC\" \"TESTSTR\"] 'Z' \"FNIDENSIEN\")"
+                .chars()
+                .collect(),
+            [
+                Token::LParen,
+                Token::LBrack,
+                Token::from("ABC"),
+                Token::from("TESTSTR"),
+                Token::RBrack,
+                Token::CharLiteral(b'Z'),
+                Token::from("FNIDENSIEN"),
+                Token::RParen,
+            ],
+        );
+        let (input2, output2) = (
+            // ''' is valid because of how I naively parse char literals
+            "(- (+ \"AIENdkfqw\" 1c) '`' \"AIENdenqiekS81\" \"))\\n\")"
+                .chars()
+                .collect(),
+            [
+                Token::LParen,
+                Token::Minus,
+                Token::LParen,
+                Token::Plus,
+                Token::from("AIENdkfqw"),
+                Token::from(NumLiteral::new_int_with_suffix(1, false, 'c')),
+                Token::RParen,
+                Token::CharLiteral(b'`'),
+                Token::from("AIENdenqiekS81"),
+                // Defining it as below to makes sure the escaping of the `\` is working
+                Token::from(String::from_iter([')', ')', '\\', 'n'])),
+                Token::RParen,
+            ],
+        );
+
+        assert_eq!(tokenize(input1)?, output1);
+        assert_eq!(tokenize(input2)?, output2);
 
         Ok(())
     }
